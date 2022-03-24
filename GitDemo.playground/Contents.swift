4c439616517e5496b24d17d00f09472f349b556b
Adrian Dominguez <adriandgzg@gmail.com>
2022-03-24 13:52:43 -0600

    mi cambio

--- GitDemo.playground/Contents.swift
+++ GitDemo.playground/Contents.swift
@@ -3,3 +3,13 @@ import UIKit
 var greeting = "Hello, playground"
 
 var arreglo  = "Mi primera modificacion de archivos"
+
+
+
+
+var contador = 0
+while (contador < 100 ) {
+    
+    print("Aqui andamos \(contador)")
+    contador ++
+}
